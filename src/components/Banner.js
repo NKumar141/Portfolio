0dc35b93c1dec4d4653ab1f8245e2e6cd05652d4
Nikhil <nk110700@gmail.com>
2023-03-24 22:36:23 +0530

    hello

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,48 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import { useState, useEffect } from "react";
 
 export const Banner = () => {
+  const toRotate = ["Flutter Developer", "Web Developer", "Computer Engineer"];
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const period = 2000;
+  const [delta, setDelta] = useState(300 - Math.random * 100);
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -8,10 +50,16 @@ export const Banner = () => {
           <Col xs={12} xl={7} md={6}>
             <span className="tagline">Hello There !</span>
             <h1>
-              {"Hi i am Nikhil"}
-              <span className="wrap">Maxeff</span>
+              {"Hi i am Nikhil "}
+              <span className="wrap">{text}</span>
             </h1>
             <p>lets see shal we ? . ok . hmm . mmhumm .</p>
+            <button>
+              <ArrowRightCircle size={30} />
+            </button>
+          </Col>
+          <Col xs={12} xl={7} md={6}>
+            <img src={headerImg} alt="Header image" />
           </Col>
         </Row>
       </Container>
