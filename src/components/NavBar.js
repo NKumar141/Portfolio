9bde1e132dad2ebe7f645c26003bf0344ca843af
Nikhil <nk110700@gmail.com>
2023-03-24 18:13:08 +0530

    yoyo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon2 from "../assets/img/web1.png";
+import navIcon3 from "../assets/img/github1.png";
 //import { HashLink } from "react-router-hash-link";
 //import { BrowserRouter as Router } from "react-router-dom";
 
@@ -74,10 +74,10 @@ export const NavBar = () => {
               <a href="https://www.linkedin.com/in/nikhil-kumar-555341203/">
                 <img src={navIcon1} alt="" />
               </a>
-              <a href="#">
+              <a href="https://maxeffcodesolutions.blogspot.com/">
                 <img src={navIcon2} alt="" />
               </a>
-              <a href="#">
+              <a href="https://github.com/NKumar141">
                 <img src={navIcon3} alt="" />
               </a>
             </div>
