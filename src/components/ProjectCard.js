118f76874e98c1b34ce369c44aa7d14be30f8292
Nikhil <nk110700@gmail.com>
2023-03-25 20:31:12 +0530

    latest

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,10 @@ import { Col } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
-    <a href="https://play.google.com/store/apps/details?id=com.maxeff.myapp.maxeffproject">
+    <a
+      href="https://play.google.com/store/apps/details?id=com.maxeff.myapp.maxeffproject"
+      target={"_blank"}
+    >
       <Col size={12} sm={6} md={4}>
         <div className="proj-imgbx">
           <img src={imgUrl} />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard.js";
-import projImg1 from "../assets/img/project-img1.png";
+import jee from "../assets/img/jee.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 
 import TrackVisibility from "react-on-screen";
@@ -10,7 +10,7 @@ export const Projects = () => {
     {
       title: "JEE Notes App",
       description: "Flutter Develpoment",
-      imgUrl: projImg1,
+      imgUrl: jee,
     },
   ];
 
