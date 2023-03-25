70a1e9a99dab07661254d61bd6653b3c3bf0aa88
Nikhil <nk110700@gmail.com>
2023-03-25 19:33:07 +0530

    welcome

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import myGif from "./ok.gif";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "../src/components/NavBar";
 import { Banner } from "./components/Banner.js";
+import { Skills } from "./components/Skills.js";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
